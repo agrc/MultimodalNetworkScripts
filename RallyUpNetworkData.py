add2f2410c8a88a07f7077b8baaafe79f8c16e04
Greg Bunce <gbunce@utah.gov>
2019-02-01 16:51:38 -0700

    create blank dataset

--- RallyUpNetworkData.py
+++ RallyUpNetworkData.py
@@ -6,16 +6,27 @@ from datetime import datetime
 
 # get the date
 today = date.today()
-strDate = str(today.month).zfill(2) + str(today.day).zfill(2) +  str(today.year)
+strDate = str(today.month).zfill(2) + str(today.day).zfill(2) +  str(today.year) 
 
+arcpy.env.workspace = 'D:\MultimodalNetwork'
+
+# get access to utrans roads and trails
+utrans_roads = 'Database Connections\DC_TRANSADMIN@UTRANS@utrans.agrc.utah.gov.sde\UTRANS.TRANSADMIN.Centerlines_Edit\UTRANS.TRANSADMIN.Roads_Edit'
+utrans_trails =  'Database Connections\DC_TRANSADMIN@UTRANS@utrans.agrc.utah.gov.sde\UTRANS.TRANSADMIN.Trails'
 
 # create new fgdb
-arcpy.CreateFileGDB_management("D:/MultimodalNetwork", "MM_NetworkDataset_" + strDate +  ".gdb")
-
+file_geodatabase = 'D:\MultimodalNetwork\MM_NetworkDataset_' + strDate +  '.gdb'
+print(file_geodatabase)
+arcpy.CreateFileGDB_management('D:\MultimodalNetwork', 'MM_NetworkDataset_' + strDate +  '.gdb')
 
 # create dataset
+arcpy.CreateFeatureDataset_management(file_geodatabase, 'NetworkDataset', utrans_roads)
 
 # create new feature class
+fgdb_dataset_name = 'D:\MultimodalNetwork\MM_NetworkDataset_' + strDate + '.gdb\NetworkDataset'
+arcpy.CreateFeatureclass_management(fgdb_dataset_name, "BikePedAuto", "POLYLINE", 
+                                    'D:\MultimodalNetwork\FeatureClassTemplate.gdb\BikePedAuto_template', "DISABLED", "DISABLED", 
+                                    utrans_roads)
 
 # add fields
 
