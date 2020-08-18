11da8e3fb3f1029015b8ec4e04269e35dc162d44
Greg Bunce <gbunce@utah.gov>
2020-08-18 17:27:00 -0600

    remove transnetwork=yes

--- rallyup_network_data.py
+++ rallyup_network_data.py
@@ -9,6 +9,7 @@ from datetime import datetime
 #: Notes before running: verify data sources (local SGID data vs SGID data -- home vpn vs at work). Variables to check:
     # utrans_roads
     # utrans_trails
+    # also, you need to be on the state network (VPN) to get the domain codes (line 91) 
 
 #### User Note ####: change dates for fgdb to current dataset (ctrl + f for "#### Note ####:")
 transit_route_source = r'C:\Users\gbunce\Documents\projects\MultimodalNetwork\MM_TransitData_02152019.gdb\TransitRoutes' #### Note ####: change transit data date (if it's been updated)
@@ -100,96 +101,99 @@ def import_RoadsIntoNetworkDataset(utrans_roads_to_import):
         # itterate though the intersected utrans road centerline features
         for utrans_row in search_cursor:
 
-            # create the network dataset values
-            source_data = 'RoadCenterlines'
-            auto_network = 'Y'
-            connector_network = 'N'
-            road_class = ''
-            aadt = 0
-            aadt_yr = ''
+            #: omit certain cartocodes from the multimodal dataset
+            if utrans_roads[7] not in ('99', '13', '14', '15', '17', '18'):
 
-            # bike network
-            bike_network = 'N'
-            bike_l = utrans_row[5]
-            bike_r = utrans_row[6]
-            cartocode = utrans_row[7]
-            #bike_facilities = ['1','1A','1B','1C','2','2A','2B'] # only roads with bike lanes or cycle tracks are added to the bike network (no shared lanes)
-            #if bike_l in bike_facilities and bike_r in bike_facilities:
-            #    bike_network = 'Y'            
-            if HasFieldValue(bike_l) or HasFieldValue(bike_r):
-                bike_network = 'Y'
-            if cartocode not in ['1', '2', '4', '7']: #: also mark local roads as bike network for wider connectivity
-                bike_network = 'Y'
-        
-            # ped network (it is assumed that anything other than 'prohibited', ped is allowed)
-            ped_network = 'Y'
-            ped_l = utrans_row[3]
-            ped_r = utrans_row[4]
-            if HasFieldValue(ped_l) or HasFieldValue(ped_r):
-                if ped_l == 'Restricted' or ped_r == 'Restricted':
-                    ped_network = 'N'
+                # create the network dataset values
+                source_data = 'RoadCenterlines'
+                auto_network = 'Y'
+                connector_network = 'N'
+                road_class = ''
+                aadt = 0
+                aadt_yr = ''
 
-            # convert meters to miles
-            miles = utrans_row[10] * 0.000621371
-            miles = round(miles, 10)
-            oneway = ''
-            if utrans_row[1] == '0':
-                oneway = "B"
-            if utrans_row[1] == '1':
-                oneway = "TF"
-            if utrans_row[2] == '2':
-                oneway = "FT"
+                # bike network
+                bike_network = 'N'
+                bike_l = utrans_row[5]
+                bike_r = utrans_row[6]
+                cartocode = utrans_row[7]
+                #bike_facilities = ['1','1A','1B','1C','2','2A','2B'] # only roads with bike lanes or cycle tracks are added to the bike network (no shared lanes)
+                #if bike_l in bike_facilities and bike_r in bike_facilities:
+                #    bike_network = 'Y'            
+                if HasFieldValue(bike_l) or HasFieldValue(bike_r):
+                    bike_network = 'Y'
+                if cartocode not in ['1', '2', '4', '7']: #: also mark local roads as bike network for wider connectivity
+                    bike_network = 'Y'
+            
+                # ped network (it is assumed that anything other than 'prohibited', ped is allowed)
+                ped_network = 'Y'
+                ped_l = utrans_row[3]
+                ped_r = utrans_row[4]
+                if HasFieldValue(ped_l) or HasFieldValue(ped_r):
+                    if ped_l == 'Restricted' or ped_r == 'Restricted':
+                        ped_network = 'N'
 
-            # transfer the speed value
-            speed_lmt = utrans_row[2]
-            if speed_lmt == 0: # and cartocode is 11
-                speed_lmt = 25
+                # convert meters to miles
+                miles = utrans_row[10] * 0.000621371
+                miles = round(miles, 10)
+                oneway = ''
+                if utrans_row[1] == '0':
+                    oneway = "B"
+                if utrans_row[1] == '1':
+                    oneway = "TF"
+                if utrans_row[2] == '2':
+                    oneway = "FT"
 
-            # calculate the time fields
-            drive_time = (miles / speed_lmt) * 60
-            ped_time = (miles / 3.1) * 60
-            #: bike time - incentivize bike lanes and local roads. roads with bike facilities, pathways, and cartocode = 11 (local roads) get a 15% faster travel speed (cost of traversing is calculated at 11 mph instead of 9.6 mph)
-            if HasFieldValue(bike_l) or HasFieldValue(bike_r) or cartocode == '11':
-                bike_time = (miles / 11) * 60
-            else:
-                bike_time = (miles / 9.6) * 60
+                # transfer the speed value
+                speed_lmt = utrans_row[2]
+                if speed_lmt == 0: # and cartocode is 11
+                    speed_lmt = 25
 
-            # transfer the road class field over
-            road_class = desc_lu[utrans_row[7]]
+                # calculate the time fields
+                drive_time = (miles / speed_lmt) * 60
+                ped_time = (miles / 3.1) * 60
+                #: bike time - incentivize bike lanes and local roads. roads with bike facilities, pathways, and cartocode = 11 (local roads) get a 15% faster travel speed (cost of traversing is calculated at 11 mph instead of 9.6 mph)
+                if HasFieldValue(bike_l) or HasFieldValue(bike_r) or cartocode == '11':
+                    bike_time = (miles / 11) * 60
+                else:
+                    bike_time = (miles / 9.6) * 60
 
-            # transfer the AADT values over
-            if utrans_row[8] is not None:
-                aadt = utrans_row[8]
+                # transfer the road class field over
+                road_class = desc_lu[utrans_row[7]]
 
-            # transfer the AADT year over
-            aadt_yr = utrans_row[9]
+                # transfer the AADT values over
+                if utrans_row[8] is not None:
+                    aadt = utrans_row[8]
+
+                # transfer the AADT year over
+                aadt_yr = utrans_row[9]
 
 
-            # create a list of values that will be used to construct new row
-            insert_row_values = [(utrans_row[0], miles, oneway, source_data, speed_lmt, drive_time, ped_time, bike_time, auto_network, ped_network, bike_network, connector_network, road_class, aadt, aadt_yr, bike_l, bike_r, utrans_row[11])]
-            print insert_row_values
+                # create a list of values that will be used to construct new row
+                insert_row_values = [(utrans_row[0], miles, oneway, source_data, speed_lmt, drive_time, ped_time, bike_time, auto_network, ped_network, bike_network, connector_network, road_class, aadt, aadt_yr, bike_l, bike_r, utrans_row[11])]
+                print insert_row_values
 
-            # insert the new row with the list of values
-            for insert_row in insert_row_values:
-                insert_cursor.insertRow(insert_row)
+                # insert the new row with the list of values
+                for insert_row in insert_row_values:
+                    insert_cursor.insertRow(insert_row)
 
                 
 # this function imports the user-defined utrans trails into the the netork dataset feature class 
 def import_TrailsIntoNetworkDataset(utrans_trails_to_import):
     
     # convert features in trail dataset from multipart to singlepart
-    trails_singlepart = MultipartToSinglepart_management(utrans_trails_to_import, 'C:\Users\gbunce\Documents\projects\MultimodalNetwork\MultimodalScratchData.gdb\trails_singlepart' + '_' + strDate)
-    
+    output = 'C:\\Users\\gbunce\\Documents\\projects\\MultimodalNetwork\\MultimodalScratchData.gdb\\trails_singlepart' + '_' + strDate
+    trails_singlepart = arcpy.MultipartToSinglepart_management(utrans_trails_to_import, output)
     
     # create list of field names
-    #                    0                1                 2            3 
-    trail_fields = ['PrimaryName', 'DesignatedUses', 'SHAPE@LENGTH', 'SHAPE@']
+    #                    0                1               2             3            4
+    trail_fields = ['PrimaryName', 'DesignatedUses', 'CartoCode', 'SHAPE@LENGTH', 'SHAPE@']
     #                   0           1             2          3           4          5              6              7            8              9             10                11            12
     network_fields = ['Name', 'Length_Miles', 'Oneway', 'SourceData', 'Speed', 'DriveTime', 'PedestrianTime', 'BikeTime', 'AutoNetwork', 'PedNetwork', 'BikeNetwork', 'ConnectorNetwork', 'SHAPE@']
 
     # set up search cursors to select and insert data between feature classes
     with arcpy.da.SearchCursor(trails_singlepart, trail_fields) as search_cursor, arcpy.da.InsertCursor(bike_ped_auto, network_fields) as insert_cursor:
-        # itterate though the intersected utrans road centerline features
+        #: itterate though the intersected utrans road centerline features
         for utrans_row in search_cursor:
 
             # create the network dataset values
@@ -200,42 +204,47 @@ def import_TrailsIntoNetworkDataset(utrans_trails_to_import):
             speed_lmt = None
             oneway = ''
 
-            # populate bike and ped netork values
-            designated_use = ''
-            bike = ''
-            ped_network = '' 
-            # domains for this field are: ['Bike', 'Pedestrian', 'Multiuse']
-            if HasFieldValue(utrans_row[1]):
-                designated_use = utrans_row[1]
-                
-            if designated_use == 'Bike':
-                bike_network = 'Y'
-                ped_network = 'N'
-            elif designated_use == 'Pedestrian':
-                bike_network = 'N'
-                ped_network = 'Y'
-            elif designated_use == 'Multiuse':
-                bike_network = 'Y'
-                ped_network = 'Y'
-            else:
-                bike_network = '?'
-                ped_network = '?'
+            #: only bring in segments with specific cartocode
+            if HasFieldValue(utrans_row[2]):
+                cartocode = utrans_row[2]
+                if cartocode in ('3 - Paved Shared Use','8 - Bridge, Tunnel','9 - Link'):
 
-            # convert meters to miles
-            miles = utrans_row[2] * 0.000621371
-            miles = round(miles, 10)
+                    #: populate bike and ped network values
+                    designated_use = ''
+                    bike = ''
+                    ped_network = '' 
+                    #: domains for this field are: ['Bike', 'Pedestrian', 'Multiuse']
+                    if HasFieldValue(utrans_row[1]):
+                        designated_use = utrans_row[1]
+                        
+                    if designated_use == 'Bike':
+                        bike_network = 'Y'
+                        ped_network = 'N'
+                    elif designated_use == 'Pedestrian':
+                        bike_network = 'N'
+                        ped_network = 'Y'
+                    elif designated_use == 'Multiuse':
+                        bike_network = 'Y'
+                        ped_network = 'Y'
+                    else:
+                        bike_network = 'U'
+                        ped_network = 'U'
 
-            # calculate the time fields
-            ped_time = (miles / 3.1) * 60
-            bike_time = (miles / 11) * 60
+                    # convert meters to miles
+                    miles = utrans_row[3] * 0.000621371
+                    miles = round(miles, 10)
 
-            # create a list of values that will be used to construct new row
-            insert_row_values = [(utrans_row[0], miles, oneway, source_data, speed_lmt, drive_time, ped_time, bike_time, auto_network, ped_network, bike_network, connector_network, utrans_row[3])]
-            print insert_row_values
+                    # calculate the time fields
+                    ped_time = (miles / 3.1) * 60
+                    bike_time = (miles / 11) * 60
 
-            # insert the new row with the list of values
-            for insert_row in insert_row_values:
-                insert_cursor.insertRow(insert_row)
+                    # create a list of values that will be used to construct new row
+                    insert_row_values = [(utrans_row[0], miles, oneway, source_data, speed_lmt, drive_time, ped_time, bike_time, auto_network, ped_network, bike_network, connector_network, utrans_row[4])]
+                    print insert_row_values
+
+                    # insert the new row with the list of values
+                    for insert_row in insert_row_values:
+                        insert_cursor.insertRow(insert_row)
 
 
 # this function returns either utrans roads or trails that intersect a specified boundary (ex: 50 sites buffer polygon) 
@@ -251,9 +260,9 @@ def get_SouceDataUsingSpatialQuery(spatial_boundary, utransFeatureClass, source)
 
     # make feature layer of utrans data (but, also use a where clause if it's trails dataset, to limit the segments to transportation trails only)
     if source == "Roads":
-        arcpy.MakeFeatureLayer_management(utransFeatureClass, 'utransIntersected_lyr', r"CARTOCODE not in ('99', '13', '14', '15', '17', '18')")
+        arcpy.MakeFeatureLayer_management(utransFeatureClass, 'utransIntersected_lyr')
     if source == "Trails":
-        arcpy.MakeFeatureLayer_management(utransFeatureClass, 'utransIntersected_lyr', r"Status = 'EXISTING' and TransNetwork = 'Yes'")
+        arcpy.MakeFeatureLayer_management(utransFeatureClass, 'utransIntersected_lyr', r"Status = 'EXISTING'")
 
     # instersect the utrans data with the fifty site buffers
     arcpy.SelectLayerByLocation_management('utransIntersected_lyr', 'intersect', 'spatialSelectPolygon_lyr')
